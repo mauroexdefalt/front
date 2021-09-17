c128b457a722adee26acb1b7baf5e81935c631b5
Mauro <exdefalt951@gmail.com>
2021-09-17 08:07:14 -0300

    primeiro commit

---
+++ pages/login/index.js
@@ -0,0 +1,11 @@
+import {Container  , Card}   from './styles'
+
+export default function Login(){
+    return(
+
+        <Container>
+            <Card>teste</Card>
+        </Container>
+
+    )
+}
---
+++ pages/login/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+height: 100vh;
+width: 100%;
+align-items: center;
+justify-content: center;
+background-color: #222;
+`
+
+
+export const  Card = styled.div`
+height: 300px;
+width: 400px;
+background-color: #fff;
+
+`
